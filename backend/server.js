b4499f869c83ac7fa943e97d49141895ca454a5f
Basheer ahmed <basheerm2006@gmail.com>
2026-04-30 15:16:30 +0530

    fix: Express 5 named wildcard parameter

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use('/api/tts', ttsRoutes);
 // Serve Frontend in Production
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('(.*)', (req, res) => {
+app.get('*path', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
